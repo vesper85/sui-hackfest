7c23dec6d8fbc9019b0d559489d567f92fee8aed
chann44 <44chansw@gmail.com>
2026-02-06 02:30:32 +0530

    server setup added

---
+++ apps/server/drizzle/0000_chilly_carmella_unuscione.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text,
+	"created_at" timestamp DEFAULT now()
+);
---
+++ apps/server/src/db/schema.ts
@@ -0,0 +1,9 @@
+
+import { pgTable, serial, text, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    name: text("name"),
+    email: text("email"),
+    createdAt: timestamp("created_at").defaultNow(),
+});
